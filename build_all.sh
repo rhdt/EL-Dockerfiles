b2deed4ff0a8c50a9eedf71c6ef0131bc7434c47
Karanbir Singh <kbsingh@gmail.com>
2018-04-12 12:40:03 +0100

    just a very small builder script (#4)
    * just a very small builder script
    
    * update to skip a rmi if no data

---
+++ build_all.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+PushReg="push.registry.devshift.net"
+PushPath="osio-prod"
+PushTag="latest"
+BaseDir=`pwd`
+
+for target in $(find . -type f -name Dockerfile | sort); do 
+  cd `dirname $target`
+  img_tag=$(pwd | sed -e "s|${BaseDir}/||g" )
+  echo $img_tag
+  target="${PushReg}/${PushPath}/${img_tag}:${PushTag}"
+  docker build -t ${target} . 
+  if [ $? -eq 0 ]; then
+    docker push $target
+  fi
+done
+
+# clean up stale images
+docker images -qf dangling=true | xargs --no-run-if-empty docker rmi
