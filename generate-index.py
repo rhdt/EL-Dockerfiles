5621faa22c38711f860df52423aa1db6de8e4efe
Jaime Melis <jmelis@redhat.com>
2018-05-25 11:52:17 +0200

    Fix generate-index.py after applying quay.io

--- generate-index.py
+++ generate-index.py
@@ -22,9 +22,9 @@ for filename in glob.glob("base/*/Dockerfile"):
 
     job = {'image_name': image_name, 'branch_name': 'master'}
 
-    if docker_from_registry == "prod.registry.devshift.net":
+    if docker_from_registry == "quay.io":
         kind = "downstream-rhel"
-        job['parent_job'] = 'rhel-%s-master' % (docker_from_image_name,)
+        job['parent_job'] = '%s-master' % (docker_from_image_name,)
     else:
         kind = "base-rhel"
 
