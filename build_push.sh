43e7ba4527d35798b832701e853bc2a9ee51471a
Jaime Melis <j.melis@gmail.com>
2018-06-05 09:32:34 +0200

    Use no-cache for builds when code changes (#59)
    New commit: build with --no-cache
    
    * GIT_PREVIOUS_COMMIT is defined
    * GIT_PREVIOUS_COMMIT != GIT_COMMIT
    
    Timer: regular build (with cache)
    
    * GIT_PREVIOUS_COMMIT is defined
    * GIT_PREVIOUS_COMMIT == GIT_COMMIT
    
    Manual: build with --no-cache
    
    * GIT_PREVIOUS_COMMIT is not defined
    
    This works the same for downstream jobs

---
+++ build_push.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+set -ex
+
+IMAGE_NAME="$1"
+
+echo "Building ${IMAGE_NAME} image"
+
+cd base/${IMAGE_NAME}/
+
+if [[ "$GIT_COMMIT" != "$GIT_PREVIOUS_COMMIT" ]]; then
+  NO_CACHE_CMD="--no-cache"
+fi
+
+DEVSHIFT_TAG="push.registry.devshift.net/osio-prod/base/${IMAGE_NAME}:latest"
+QUAY_TAG="quay.io/openshiftio/rhel-base-${IMAGE_NAME}:latest"
+
+docker build --pull "${NO_CACHE_CMD}" \
+  -t "${DEVSHIFT_TAG}" \
+  -t "${QUAY_TAG}" \
+  .
+
+docker push "${DEVSHIFT_TAG}"
+docker push "${QUAY_TAG}"
