753fe3fe51aa8893bafda6f6e946898116f1415f
Michal Srb <michal@redhat.com>
2019-05-10 19:09:02 +0200

    s/python3-pip/python2-pip/

--- base/fabric8-analytics-worker/hack/install_deps_rpm.sh
+++ base/fabric8-analytics-worker/hack/install_deps_rpm.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/sh -e
 
 # Required by Dockerfile or any built-time script in hack/
-BUILD="python36-pip python3-pip wget which"
+BUILD="python36-pip python2-pip wget which"
 
 # - We need postgresql-devel and python3-devel for psycopg2 listed in f8a_worker/requirements.txt
 # - We cannot use requests from PyPI since it does not allow us to use own certificates
