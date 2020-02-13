c9617683f6d2a2ebf3a67964254166feb1198735
Serhii Kryzhnii <skryzhni@redhat.com>
2020-02-13 15:50:09 +0200

    Change docker push to skopeo copy

--- build_push.sh
+++ build_push.sh
@@ -20,4 +20,8 @@ docker build --pull "${NO_CACHE_CMD}" \
   -t "${QUAY_TAG}" \
   .
 
-docker push "${QUAY_TAG}"
+#docker push "${QUAY_TAG}"
+# push the image
+skopeo copy --dest-creds "${QUAY_USER}:${QUAY_TOKEN}" \
+    "docker-daemon:${QUAY_TAG}" \
+    "docker://${QUAY_TAG}"
